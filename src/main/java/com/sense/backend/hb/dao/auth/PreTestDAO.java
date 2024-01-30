cad347471dfd83632a434fc61d11d5c215164642
Kitdanai <s.smilekitdanai@gmail.com>
2024-01-30 13:00:46 +0700

    create PretestDAO

---
+++ src/main/java/com/sense/backend/hb/dao/auth/PreTestDAO.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sense.backend.hb.dao.auth;
+
+import com.sense.backend.hb.dao.BaseDAO;
+import com.sense.backend.hb.entity.auth.PreTestEntity;
+
+/**
+ *
+ * @author SenseInfoTech
+ */
+public class PreTestDAO extends BaseDAO<PreTestEntity, Integer>{
+    
+    public PreTestDAO(){
+        super(PreTestEntity.class);
+    }
+    
+}
