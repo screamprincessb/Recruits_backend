a8f6249a9d300de3f2e26b772c7b13903f809e54
Kitdanai <s.smilekitdanai@gmail.com>
2024-01-30 11:52:42 +0700

    Create preTestEntity

---
+++ src/main/java/com/sense/backend/hb/entity/auth/PreTestEntity.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sense.backend.hb.entity.auth;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import lombok.Data;
+
+/**
+ *
+ * @author SenseInfoTech
+ */
+@Data
+@Entity
+@Table(name = "PRETEST")
+public class PreTestEntity implements Serializable {
+    
+    @Id
+    @Column(name = "PRE_TEST_ID")
+    private int preTestId;
+    
+    @Column(name = "PRE_TEST_QUESTION")
+    private String preTestQuestion;
+    
+    @Column(name = "PRE_TEST_CHOICE")
+    private String preTestChoice;
+    
+    @Column(name = "PRE_TEST_CORRECT")
+    private char preTestCorrect;
+    
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "PRE_TEST_CREATE_DATE")
+    private Date preTestCreateDT;
+    
+    @Column(name = "PRE_TEST_CREATE_BY")
+    private String preTestCreateBy;
+    
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "PRE_TEST_UPDATE_DATE")
+    private Date preTestUpdateDT;
+    
+    @Column(name = "PRE_TEST_UPDATE_BY")
+    private String preTestupdateBy;
+    
+    
+    
+}
